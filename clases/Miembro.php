c4756d7b464edc63b4c2a2b4589c5a4bdc55ed82
Manel <manelmoralesrivera@gmail.com>
2024-10-23 08:45:49 +0200

    Realización de la clase abstracta Miembro.php

---
+++ clases/Miembro.php
@@ -0,0 +1,40 @@
+<?php
+    abstract class Miembro{
+        private $id=0;//tiene que ser un int
+        private $nombre;
+        private $apellidos;
+        private $email;
+        private $edad=0;//tiene que ser un int
+
+        public function __construct($id,$nombre,$apellidos,$email,$edad)
+        {
+            $this->id=$id;
+            $this->nombre=$nombre;
+            $this->apellidos=$apellidos;
+            $this->email=$email;
+            $this->edad=$edad;
+        }
+
+        public function getId(){return $this->id; }
+        private function setId($id){ $this->id=$id; }
+        public function getNombre(){return $this->nombre; }
+        private function setNombre($nombre){ $this->nombre=$nombre; }
+        public function getApellidos(){return $this->apellidos;}
+        private function setApellidos($apellidos){$this->apellidos=$apellidos;}
+        public function getEmail(){return $this->email;}
+        private function setEmail($email){ $this->email=$email; }
+        public function getEdad(){return $this->edad;}
+        private function setEdad($edad){ $this->edad=$edad;}
+
+        public function __toString()
+        {
+            return "Miembro con ID: ".$this->getId()." Nombre: ".$this->getNombre()." Apellidos: ".$this->getApellidos()." Email: ".$this->getEmail()." Edad: ".$this->getEdad();
+        }
+
+
+
+
+
+    }
+
+?>
